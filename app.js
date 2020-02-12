44eb393964984da0c6778faadfbbdf43ea179f93
minavukovic <mivukovi@ucsd.edu>
2020-02-11 16:26:05 -0800

    building plans list

--- app.js
+++ app.js
@@ -42,13 +42,55 @@ if ('development' == app.get('env')) {
 app.get('/home', index.view);
 app.get('/add/shortcut', shortcut.view);
 app.get('/add/report', report.view);
-app.get('/add/building-plan', building_plan.view);
+// app.get('/add/building_plan', building_plan.view);
+app.get('/add/building_plan', building_plan.addBuildingPlan);
 app.get('/help', help.view);
 app.get('/', login.view);
 app.get('/login', login.view);
+
+
 // Example route
 // app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+////////////////////////////////////////////////////////
+// var multer  =   require('multer');
+
+// var storage =   multer.diskStorage({
+//   // file upload destination
+//   destination: function (req, file, callback) {
+//     callback(null, './uploads');
+//   },
+//   filename: function (req, file, callback) {
+//     callback(null, file.fieldname + '-' + Date.now());
+//   }
+// });
+
+// var upload = multer({ storage : storage}).single('avatar');app.get('/',function(req,res){
+//       res.sendFile(__dirname + "/building_plan.handlebars");
+// });
+
+// app.post('/add/building_plan',function(req,res){
+//     upload(req,res,function(err) {      
+//     // req.file is the `avatar` file
+//       // req.body will hold the text fields, if there were any        
+//       if(err) {
+//             return res.end("Error uploading file.");
+//             //return res.render('building_plan');
+//         }
+//         //res.end("File is uploaded");
+//         res.render('building_plan');
+//     });
+// });
+
+// app.listen(app.get('port'),function(){
+//     console.log("Working on port 3000");
+// });
+
+
+
