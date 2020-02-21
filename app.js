4aaf7038950a427ccd219ea672c8a1f7207c6cdc
lynation <shly@ucsd.edu>
2020-02-20 21:02:08 -0800

    added fb login, report cops

--- app.js
+++ app.js
@@ -1,57 +1,56 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var handlebars = require('express3-handlebars')
+var express = require("express");
+var http = require("http");
+var path = require("path");
+var handlebars = require("express3-handlebars");
 
-var index = require('./routes/index');
-var shortcut = require('./routes/shortcut');
-var report = require('./routes/report');
-var building_plan = require('./routes/building_plan');
-var help = require('./routes/help');
-var login = require('./routes/login');
+var index = require("./routes/index");
+var shortcut = require("./routes/shortcut");
+var report = require("./routes/report");
+var building_plan = require("./routes/building_plan");
+var help = require("./routes/help");
+var login = require("./routes/login");
 // Example route
 // var user = require('./routes/user');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 app.use(express.favicon());
-app.use(express.logger('dev'));
+app.use(express.logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
+app.use(express.cookieParser("IxD secret key"));
 app.use(express.session());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // development only
-if ('development' == app.get('env')) {
+if ("development" == app.get("env")) {
   app.use(express.errorHandler());
 }
 
-app.get('/home', index.view);
-app.get('/add/shortcut', shortcut.view);
-app.get('/add/report', report.view);
+app.get("/home", index.view);
+app.get("/add/shortcut", shortcut.view);
 // app.get('/add/building_plan', building_plan.view);
-app.get('/add/building_plan', building_plan.addBuildingPlan);
-app.get('/help', help.view);
-app.get('/', login.view);
-app.get('/login', login.view);
+app.get("/add/building_plan", building_plan.addBuildingPlan);
+app.get("/help", help.view);
+app.get("/", login.view);
+app.get("/login", login.view);
 
+app.get("/add/report", report.addCop);
 
 // Example route
 // app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get("port"), function () {
+  console.log("Express server listening on port " + app.get("port"));
 });
--- routes/report.js
+++ routes/report.js
@@ -1,8 +1,20 @@
-
 /*
  * GET home page.
  */
+var data = require("../data.json");
 
-exports.view = function(req, res){
-  res.render('report');
+exports.addCop = function (req, res) {
+  var x = req.query.x;
+  var y = req.query.y;
+
+  var newFriend = { "x": x, "y": y };
+
+  console.log(x);
+  console.log(y);
+  data.police.push(newFriend);
+  res.render("report", data);
 };
+
+/*exports.view = function(req, res){
+  res.render('report');
+};*/
