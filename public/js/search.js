cf6b52dd174fd3a3c48b5cf8280423ccbcef392b
lynation <shly@ucsd.edu>
2020-02-26 18:16:24 -0800

    fixed the styling

--- public/js/search.js
+++ public/js/search.js
@@ -1,21 +1,21 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function searchDestination() {
-    document.getElementById("search_dropdown").classList.toggle("show");
+  document.getElementById("search_dropdown").classList.toggle("show");
 }
 
 function filterDestination() {
-    var input, filter, a, i;
-    input = document.getElementById("loc_input");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("search_dropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        txtValue = a[i].textContent || a[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
+  var input, filter, a, i;
+  input = document.getElementById("loc_input");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("search_dropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
+  }
 }
