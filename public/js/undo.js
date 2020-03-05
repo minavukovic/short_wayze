1b5dae225ea07d3f074926be85a915b07dbca2e6
Calvin Tam <c6tam@ucsd.edu>
2020-03-05 02:02:00 -0800

    no undo when testing

--- public/js/undo.js
+++ public/js/undo.js
@@ -1,7 +1,8 @@
 function add_undo() {
     var btnHTML;
 
-    if (window.location.search !== '' && window.location.pathname.indexOf('undo') <= -1) {
+    if (window.location.search !== '' && window.location.pathname.indexOf('undo') <= -1 &&
+        !window.location.search.includes("utm_expid")) {
         btnHTML = '<button type="button" class="btn btn-dark"' +
             'onclick="window.location.href=\'' +
             window.location.pathname + '/undo' + window.location.search + '\'" ' +
