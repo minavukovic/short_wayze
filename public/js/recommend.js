7ae0237655ddef445f81dbda743071311e07cf2e
lynation <shly@ucsd.edu>
2020-03-05 19:13:21 -0800

    made other locations on main-page clickable

--- public/js/recommend.js
+++ public/js/recommend.js
@@ -18,4 +18,44 @@ $(document).ready(function() {
   $(".sun-god-closeButton").click(function() {
     $(".sun-god-hover").hide();
   });
+
+  $(".bear-popup").click(function() {
+    $(".bear-hover").show();
+  });
+  $(".bear-hover").click(function() {
+    $(".bear-hover").hide();
+  });
+  $(".bear-closeButton").click(function() {
+    $(".bear-hover").hide();
+  });
+
+  $(".triton-popup").click(function() {
+    $(".triton-hover").show();
+  });
+  $(".triton-hover").click(function() {
+    $(".triton-hover").hide();
+  });
+  $(".triton-closeButton").click(function() {
+    $(".triton-hover").hide();
+  });
+
+  $(".star-popup").click(function() {
+    $(".star-hover").show();
+  });
+  $(".star-hover").click(function() {
+    $(".star-hover").hide();
+  });
+  $(".star-closeButton").click(function() {
+    $(".star-hover").hide();
+  });
+
+  $(".sixtyfour-popup").click(function() {
+    $(".sixtyfour-hover").show();
+  });
+  $(".sixtyfour-hover").click(function() {
+    $(".sixtyfour-hover").hide();
+  });
+  $(".sixtyfour-closeButton").click(function() {
+    $(".sixtyfour-hover").hide();
+  });
 });
